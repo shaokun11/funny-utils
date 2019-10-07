636748fb3db922abbccc48fb778b7c59a9d279f6
shaokun <40231565+shaokun11@users.noreply.github.com>
2019-10-07 23:16:08 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,12 @@ const path = require('path');
 module.exports = {
     entry: './src/index.ts',
     output: {
-        filename: "funny-utils.min.js",
-        library: 'funnyUtils',
-        libraryTarget: 'umd',
+        filename: "funny-utils.min.js", //打包之后生成的文件名，可以随意写。
+        library: 'funnyUtils',  // 指定类库名,主要用于直接引用的方式(比如使用script 标签)
+        libraryTarget: 'umd',   // 定义打包方式Universal Module Definition,同时支持在CommonJS、AMD和全局变量使用
         path: path.resolve(__dirname, 'dist'),
-        globalObject: "this",
+        globalObject: "this",   // 定义全局变量,兼容node和浏览器运行，避免出现"window is not defined"的情况
+        libraryExport: "default", // 对外暴露default属性，就可以直接调用default里的属性
     },
     mode: 'production',
     module: {
